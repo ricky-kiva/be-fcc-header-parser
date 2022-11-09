f050bfcc58ad9bb7ed07d4eef99d03c2440c82c1
Rickyslash <85386584+ricky-kiva@users.noreply.github.com>
2022-11-09 17:13:28 +0000

    finishing the project

--- index.js
+++ index.js
@@ -20,8 +20,17 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res, next) => {
+  req.ipaddress = req.header('x-forwarded-for');
+  req.software = req.header('User-Agent');
+  req.language = req.header('Accept-Language');
+  next();
+}, (req, res, next) => {
+  res.json({
+    'ipaddress': req.ipaddress,
+    'language': req.language,
+    'software': req.software
+  })
 });
 
 // listen for requests :)
